786434f5e3ef3e7ebc779652bf8dd37c6dceed18
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-10-31 14:52:42 +0000

    Add files via upload

---
+++ Part1/lesson 2.7/JavaScript6.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById("number").innerHTML = Number(true) + "<br>" + Number(false) + "<br>"
++ Number("1") + "<br>" + Number("999 888");
+
+
+document.getElementById("string").innerHTML = String("String") + "<br>" +  String(new Date()) +
+    "<br>" + String(111);
+
+
+
