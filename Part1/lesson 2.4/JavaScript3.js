3626082851344d93a2443b5f483467601ecfb1eb
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-10-31 13:45:38 +0000

    Add files via upload

---
+++ Part1/lesson 2.4/JavaScript3.js
@@ -0,0 +1,10 @@
+let name = "John";
+document.write(name)//displayed John
+name = "John";
+name = "Patrick";
+document.write(name)// here is an example that if we want to use the same name of variable we can't use keyword let
+//becasue it's already declared and name displayed will be Patrick, because keyword "let" can be changed
+
+const movie = "Fast and furious";
+
+//there will be an error  : movie = 'Batman'; // error, can't reassign the constant!
