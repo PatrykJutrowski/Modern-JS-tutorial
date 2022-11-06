66f8fcab907c03c1bb6c560577ef31d33a287ef2
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-06 13:17:15 +0000

    Add files via upload

---
+++ Part1/lesson 2.8/Javascript7.js
@@ -0,0 +1,14 @@
+var x = 5, y = 10;
+
+var z = x + y; //performs addition and returns 15
+document.write(z + " Addition"+"<br>")
+z = y - x; //performs subtraction and returns 5
+document.write(z + " Subtraction"+ "<br>")
+z = x * y; //performs multiplication and returns 50
+document.write(z + " Multiplication"+ "<br>")
+z = y / x; //performs division and returns 2
+document.write(z + " Division"+ "<br>")
+z = x % 2; //returns division remainder 1
+document.write(z + " division remainder"+ "<br>")
+z = x ** y; // Exponentiation
+document.write(z + " Exponentiation"+ "<br>")
