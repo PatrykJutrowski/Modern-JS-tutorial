39c542c9058bdfdb48da1225f8d30a5597ca633f
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-07 23:45:46 +0000

    Add files via upload

---
+++ Part1/lesson 2.9/Javascript8.js
@@ -0,0 +1,11 @@
+const a = 10
+const b = 5
+const c = 5
+const d = 5
+
+
+alert( a > b ); //returns true (boolean)
+alert( a < b ); // returns false (boolean)
+alert( 'B' > 'P' ); // false
+alert( 'B' < 'P' ); //true
+alert(c === d); //true  becasue they are equal
