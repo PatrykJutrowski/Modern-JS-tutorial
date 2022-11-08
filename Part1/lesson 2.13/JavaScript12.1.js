cbead3dea6021ae6e8d28320d1ea5f7de181f217
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-08 13:10:38 +0000

    Add files via upload

---
+++ Part1/lesson 2.13/JavaScript12.1.js
@@ -0,0 +1,14 @@
+//let loading = 0;
+
+//while(loading < 15){
+   // document.write(loading) this loop will go forever
+//}
+//to stop our loop we can increment ++ to break the loop after our loading be smaller than 15
+
+let loading = 0;
+
+while (loading < 15){
+    document.write(loading + "<br>");
+    loading++;
+}
+
---
+++ Part1/lesson 2.13/JavaScript12.js
@@ -0,0 +1,13 @@
+const names = ["Ed", "John", "Paul", "Jake", "Logan", "Harry", "Ron"];
+
+
+for (name of names){
+    document.write(name + "<br>")
+    if (name==="Logan"){
+        document.write(name + " is in My list!")
+        break;
+    }
+}
+
+//let's search for name Logan
+
