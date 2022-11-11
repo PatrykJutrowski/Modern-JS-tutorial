babfc72167e97efa2079f76815339b0f191716a3
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-11 15:04:43 +0000

    Add files via upload

---
+++ Part1/lesson 2.17/JavaScript2.17.js
@@ -0,0 +1,10 @@
+hello = function() {
+    alert("Before arrow function")
+}
+hello();
+hello1= () => {
+    alert("After arrow function")
+}
+hello1();
+
+//As you can see arrow function gives us shorter version of creating function it's some  type of expression 
