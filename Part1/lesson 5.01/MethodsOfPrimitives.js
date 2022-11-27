93ac412d9f5a1f318d52f406f95a2335811491d5
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-27 13:06:28 +0000

    Add files via upload

---
+++ Part1/lesson 5.01/MethodsOfPrimitives.js
@@ -0,0 +1,8 @@
+let length = 16;                               // Number
+let lastName = "Johnson";                      // String
+let x = {firstName:"John", lastName:"Doe"};    // Object
+
+
+document.write(length + " Number" + "<br>")
+document.write(lastName + " String" + "<br>")
+document.write(x + " object")
