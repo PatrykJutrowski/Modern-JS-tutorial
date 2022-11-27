e66d50ca02ae3ca65912f87a08f37db7e00482fc
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-27 17:23:05 +0000

    Add files via upload

---
+++ Part1/lesson 5.03/Strings5.03.js
@@ -0,0 +1,11 @@
+let newString = "Some text with double quotes " +
+    "\"\" "
+let newString2 = 'Some text with single quotes' +'\'\''
+
+let newString3 = `Some text with backticks` + `\`\``
+
+
+document.write(newString + "<br>")
+document.write(newString2+ "<br>")
+document.write(newString3+ "<br>")
+
