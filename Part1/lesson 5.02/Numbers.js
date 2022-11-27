d9405552315a3a00a44e16c62a6a8a1cec4fc417
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-27 14:32:42 +0000

    Add files via upload

---
+++ Part1/lesson 5.02/Numbers.js
@@ -0,0 +1,12 @@
+let num = 1
+let newNum = 1.16
+document.write(typeof num +"<br>");
+document.write(typeof newNum +"<br>");
+
+
+let mill = 1e6; // 1 * (10**6) = 1000000;
+document.write(mill +"<br>")
+
+
+let smallNum = 1e-6
+document.write(smallNum +"<br>")
