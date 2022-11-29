464e09670eac70f432d8eec8c2291f68af589768
PatrykJutrowski <108734543+PatrykJutrowski@users.noreply.github.com>
2022-11-29 18:35:28 +0000

    Add files via upload

---
+++ Part1/lesson 5.09/ObjectKeysValuesEntries.js
@@ -0,0 +1,18 @@
+const hero = {
+    name: 'Batman',
+    city: 'Gotham'
+};
+Object.keys(hero); // => ['name', 'city']
+
+
+const hero1 = {
+    name1: 'Batman',
+    city1: 'Gotham'
+};
+Object.values(hero1); // => ['Batman', 'Gotham']
+
+const hero2 = {
+    name2: 'Batman',
+    city2: 'Gotham'
+};
+Object.entries(hero2); // => `[['name', 'Batman'], ['city', 'Gotham']]`
